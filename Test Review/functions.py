6bbb7da11675b355a7b250c5094eb527f107e3ca
parkerteeples <128662811+parkerteeples@users.noreply.github.com>
2024-10-08 09:55:50 -0600

    Test Review

---
+++ Test Review/functions.py
@@ -0,0 +1,16 @@
+def calc_triangle_area(height, base, length):
+    area = (height * base)/2
+    volume = area * length
+
+    print("Volume: ", volume)
+
+def main():
+
+    h = 3
+    b = 4
+    l = 10
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ Test Review/typecast.py
@@ -0,0 +1,9 @@
+value_1 = 10    # int
+
+value_2 = '10'  # string
+
+value_3 = 10.0  # float
+
+print("Number: " + str(value_1))    # can only concatonate strings
+
+number = int(input('Enter Number: '))
