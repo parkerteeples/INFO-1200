12bc68ae7892567a34fd8521cb1ff575943eeba5
parkerteeples <128662811+parkerteeples@users.noreply.github.com>
2024-12-05 10:56:05 -0700

    12-5-24
    Worked on Idle Game and project 10

--- Projects/IdleGame.py
+++ Projects/IdleGame.py
@@ -2,32 +2,32 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter import messagebox
 import csv
-import time
 
-currency = 0
-
-def currencyupdate(currency):
-    currency += 1
+def currencyupdate():
+    currencyVar.set(currencyVar.get() + 1)
     game_window.after(1000, currencyupdate)
-    return currency
-
 
 def UpgradeMulti():
     print('Works')
 
-
+def main():
+    currencyVar.set(0)
+    currencyupdate()
 
 
 game_window = tk.Tk()
 game_window.title("Idle Game")
 
 currencyVar = tk.IntVar()
+multiplierVar = tk.IntVar()
 
-currencylabel = ttk.Label(game_window, text='Currency: ').grid(column=0, row=0, padx=10, pady=10)
-currentamount = ttk.Entry(game_window, width=25, textvariable=currency, state='readonly').grid(column=1, row=0, padx=10, pady=10)
-multiplierbutton = ttk.Button(game_window, text='Upgrade Multiplier', command=UpgradeMulti).grid(column=0, row=1, padx=10, pady=10)
+currencylabel = ttk.Label(game_window, text='Money: ').grid(column=0, row=0, padx=5, pady=10)
+currentamount = ttk.Entry(game_window, width=25, textvariable=currencyVar, state='readonly').grid(column=1, row=0, padx=5, pady=10)
 
+mulitplierlabel = ttk.Label(game_window, text="Multiplier: ").grid(column=1, row=1, padx=5, pady=10)
+multiplierbutton = ttk.Button(game_window, text='Upgrade Multiplier', command=UpgradeMulti).grid(column=0, row=1, padx=5, pady=10)
+multiplieramount = ttk.Entry(game_window, width=25, textvariable=multiplierVar, state="readonly").grid(column=2, row=1, padx=5, pady=10)
 
-
-currencyupdate(currency)
-game_window.mainloop()
+if __name__ == "__main__":
+    main()
+    game_window.mainloop()
